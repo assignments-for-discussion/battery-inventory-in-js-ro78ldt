d4dda6ea2693255d97ccae7fc602c55b8ff405ff
ro78ldt <roy.pratim78@gmail.com>
2023-11-21 18:55:10 +0530

    Changes made

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 const assert = require('assert');
 
 function countBatteriesByHealth(presentCapacities) {
+  
+  let healthyBatteries = 0;
+  let exchangeBatteries = 0;
+  let failedBatteries = 0;
+  
+function computeSOH(presentCapacity) {
+  let ratedCapacity = 120;
+  return (presentCapacity / ratedCapacity) * 100;
+}
+ 
+for (const presentCapacity of presentCapacities) {
+  const soh = computeSOH(presentCapacity);
+  if (soh > 80) {
+    healthyBatteries++;
+  } else if (soh >= 62) {
+    exchangeBatteries++;
+  } else {
+    failedBatteries++
+  }
+}
   return {
-    healthy: 0,
-    exchange: 0,
-    failed: 0
+    healthy: healthyBatteries,
+    exchange: exchangeBatteries,
+    failed: failedBatteries,
   };
 }
+
 
 function testBucketingByHealth() {
   console.log('Counting batteries by SoH...');
